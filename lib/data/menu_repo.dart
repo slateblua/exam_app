e0612f208cde10490349672313f32854a8e390bc
Sergey Hayriyan <sega1ness@gmail.com>
2025-10-25 11:57:11 +0400

    Remove comments

--- lib/data/menu_repo.dart
+++ lib/data/menu_repo.dart
@@ -11,13 +11,11 @@ abstract interface class MenuRepo {
 class MenuRepoImpl implements MenuRepo {
   @override
   Future<List<MenuItem>> fetchMenuItems(int page) async {
-    // Simulate network delay
     await Future.delayed(Duration(seconds: 1));
 
     final int start = page * 5;
     final int end = min((page + 1) * 5, FakeData.menuItems.length);
 
-    // Generate dummy menu items
     List<MenuItem> menuItems = start >= end ? [] : FakeData.menuItems.getRange(start, end).toList();
 
     return menuItems;
